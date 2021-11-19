0cf1344f10073bd4b50fb164cbc09fecdde3398d
juanpablo-git <salesjuan781@gmail.com>
2021-11-19 14:45:08 -0300

    config banco

--- node/nlwValoriza/src/repositores/UserRepositores.ts
+++ node/nlwValoriza/src/repositores/UserRepositores.ts
@@ -1,2 +1,8 @@
 import { EntityRepository,Repository } from "typeorm";
-import { User } from "../../user";
+import { User } from "../entities/User";
+
+@EntityRepository(User)
+class UsersRepositores extends Repository<User>{
+
+}
+export {UsersRepositores}
