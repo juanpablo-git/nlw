cacf09307331ee14584435cfcf5d9c1bce8cd594
juanpablo-git <salesjuan781@gmail.com>
2021-11-22 09:01:11 -0300

    resdulvendo erros assincronos

--- node/node/nlwValoriza/src/server.ts
+++ node/node/nlwValoriza/src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express,{Request,Response,NextFunction, response} from 'express'
+import "express-async-errors"
 import "reflect-metadata";
 import "./database";
 import { router } from './rounts/router';
@@ -7,5 +8,18 @@ import { router } from './rounts/router';
 const app = express()
 app.use(express.json())
 app.use(router)
+app.use((err:Error,req:Request,res:Response,next:NextFunction)=>{
+    if(err instanceof Error) {
+        return res.status(400).json({
+            error:err.message
+            
+        })
+    }
 
+    return response.status(500).json({
+        status:"error",
+        mensage:"Internal error"
+    })
+
+})
 app.listen(3000,()=>console.log("Server runing"))
